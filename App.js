836b961c78ca4d91e895c9ea9c25f5afc114e1eb
Long Luu <luuhoanglong577@gmail.com>
2022-01-04 11:36:25 +0700

    add line image

--- App.js
+++ App.js
@@ -19,8 +19,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {/* Today's Task */}
       <Image style={styles.logo} source={require("./assets/task1.png")} />
+      {/* Today's Task */}
       <View style={styles.tasksWrapper}>
         <Text style= {styles.sectionTitle}>Today's tasks</Text>
         <View style={styles.items}>
